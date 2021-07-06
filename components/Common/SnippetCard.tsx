dab8280617abfee3ebf0f7ee18f65bea5c0c0dc5
Zac <zilson19878@gmail.com>
2021-07-05 20:06:09 -0500

    initialize

--- components/Common/SnippetCard.tsx
+++ components/Common/SnippetCard.tsx
@@ -1,6 +1,6 @@
 export const SnippetCard = () => {
     return (
-        <div className="border-2 rounded-md border-red max-w-sm">
+        <div className="border-2 rounded-md border-red max-w-sm h-1">
             <div className=" bg-blue-300 text-black-200 rounded-t-md grid grid-cols-2">
                 <h3 className="justify-self-start ml-2">BS</h3>
                 <div className="justify-self-end">
