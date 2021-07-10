be7531752de3cfa537c82271b2af4e7b35809cf3
xamthor <miguel@mhernandez.me>
2021-07-10 10:41:40 -0500

    hero data ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,18 @@ import { CommunityStats } from '../components/site/community-stats';
 import { Hero } from '../components/site/hero'
 
 export default function Index() {
+	
+	const heroData = {
+	    title: "Browse and create code snippets",
+	    subtitle: "Snippets are small pieces of code you don't need to remember.",
+	    buttonText: "Checkout our example"
+	}
 
     return (
         <>
             <Layout>
                 <main className="flex flex-col flex-grow items-center w-full text-center">
-                    <Hero 
-                    title="Browse and create code snippets" 
-                    subtitle="Snippets are small pieces of code you don't need
-                    to remember."
-                    buttonText="Checkout our example"/>
+                    <Hero { ... heroData}/>
                     <CommunityStats/>
                 </main>
             </Layout>
