ad1f467ba5f5b2fa951bb292c6cea5685af5369e
xamthor <miguel@mhernandez.me>
2021-07-13 13:30:17 -0500

    set page title

--- components/Common/Layout.tsx
+++ components/Common/Layout.tsx
@@ -25,7 +25,7 @@ export const Layout = ({ children, protectedRoute }: Layout) => {
         <div className="relative min-h-screen py-2">
             <div className="container flex flex-col min-h-full mx-auto">
                 <Head>
-                    <title>Create Next App</title>
+                    <title>Code Parcel</title>
                     <link rel="icon" href="/favicon.ico" />
                     <link
                         rel="preconnect"
