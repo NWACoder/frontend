a8fea9a62fa7c7b458a9d96e364a72ada2abe4bf
Jesse <moore.jesse@gmail.com>
2021-07-13 19:49:39 -0500

    fixed error when community-stats api returns no data

--- components/site/community-stats.tsx
+++ components/site/community-stats.tsx
@@ -3,49 +3,40 @@ import { IndexState } from '../../types';
 import { getCommunityStats } from '../../api/site/stats';
 
 export const CommunityStats = () => {
-	
-	const [state, setState] = useState<IndexState>({
-		Users: 0,
-		Snippets: 0,
-		Challenges: 0,
-	});
+    const [state, setState] = useState<IndexState>({
+        Users: 0,
+        Snippets: 0,
+        Challenges: 0,
+    });
 
-	useEffect(() => {
-	    const communityStats = async () => {
-	    	const res = await getCommunityStats();
-	    	setState(res.value);
-	    };
-	    communityStats();
-	}, []);
+    useEffect(() => {
+        const communityStats = async () => {
+            const data = await getCommunityStats();
+            if (!data) return;
+            setState(data.value);
+        };
+        communityStats();
+    }, []);
 
-	const { Users, Snippets, Challenges } = state;
+    const { Users, Snippets, Challenges } = state;
 
-	return (
-		<div className="mt-16">
-		    <div className="text-2xl font-thin sm:my-8">
-				Community Stats
-		    </div>
-		    <div className="sm:flex sm:flex-row sm:flex-wrap">
-		        <div className="sm:border-r sm:border-b-0 sm:px-16 sm:py-0 py-8 border-b max-w-max mx-auto">
-		            <div className="text-2xl font-semibold">
-		                {Users}
-		            </div>
-		            <div className="font-thin mt-2">Users</div>
-		        </div>
-		        <div className="sm:border-r sm:border-b-0 sm:px-16 sm:py-0 py-8 border-b max-w-max mx-auto">
-		            <div className="text-2xl font-semibold">
-		                {Snippets}
-		            </div>
-		            <div className="font-thin mt-2">Snippets</div>
-		        </div>
-		        <div className="sm:px-16 sm:py-0 py-8 max-w-max mx-auto">
-		            <div className="text-2xl font-semibold">
-		                {Challenges}
-		            </div>
-		            <div className="font-thin mt-2">Q&A</div>
-		        </div>
-		    </div>
-		</div>
-	)
-}
-
+    return (
+        <div className="mt-16">
+            <div className="text-2xl font-thin sm:my-8">Community Stats</div>
+            <div className="sm:flex sm:flex-row sm:flex-wrap">
+                <div className="sm:border-r sm:border-b-0 sm:px-16 sm:py-0 py-8 border-b max-w-max mx-auto">
+                    <div className="text-2xl font-semibold">{Users}</div>
+                    <div className="font-thin mt-2">Users</div>
+                </div>
+                <div className="sm:border-r sm:border-b-0 sm:px-16 sm:py-0 py-8 border-b max-w-max mx-auto">
+                    <div className="text-2xl font-semibold">{Snippets}</div>
+                    <div className="font-thin mt-2">Snippets</div>
+                </div>
+                <div className="sm:px-16 sm:py-0 py-8 max-w-max mx-auto">
+                    <div className="text-2xl font-semibold">{Challenges}</div>
+                    <div className="font-thin mt-2">Q&A</div>
+                </div>
+            </div>
+        </div>
+    );
+};
