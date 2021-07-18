ca3e468c4b78b6d55faab9d0b0f072de97be0fd9
xamthor <miguel@mhernandez.me>
2021-07-18 09:14:25 -0500

    test env

--- lib/axios/index.ts
+++ lib/axios/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+
+const baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL || process.env.SERVER_BASE_URL;
 export const authAxios = () => {
     const token = localStorage.getItem('codeParcelUserToken');
     if (token === null) throw new Error('Unauthorized');
